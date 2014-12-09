081e62613157608d0fd22c1bd04b8b28bb63ef15
lbarberiscanoni <hllbck7@gmail.com>
2014-12-09 15:37:31 -0500

    fixed the script but it still needs smoothing

--- td_bank/scripts/dropdown_menu.js
+++ td_bank/scripts/dropdown_menu.js
@@ -1,3 +1,13 @@
-$("section.top button").click(function(){
-$("nav li").toggleClass("drop_down");
+$(".menu").click(function(){
+	console.log("clicked");
+  if ($("nav").css("height") === "200px") {
+    $("nav").css({height: "0"});
+    $("ul").css({opacity: "0"});
+    console.log("if works");
+
+  } else {
+    $("nav").css({height: "200px", transition: "height 1000ms"});
+    $("ul").css({opacity: "1", transition: "opacity 1000ms"});
+    console.log("else works");
+  }
 });
