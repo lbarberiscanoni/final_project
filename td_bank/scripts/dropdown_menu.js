150e224a44fec8f08ec00ad0890448128f8f86fd
lbarberiscanoni <hllbck7@gmail.com>
2014-12-10 14:46:53 -0500

    improved the js for the nav and messaged board

--- td_bank/scripts/dropdown_menu.js
+++ td_bank/scripts/dropdown_menu.js
@@ -3,14 +3,15 @@ $(".menu").click(function(){
 
   if ($("nav").css("height") === "160px") {
     console.log("if works");
-    $("nav").css({display: "block"});
     $("nav").css({height: "0", transition: "height 1000ms"});
     $("ul").css({opacity: "0", transition: "opacity 1000ms"});
+    $("nav").css({display: "none"});
     $(".main").css({opacity: "1"});
 
   } else {
     console.log("else works");
     $(".main").css({opacity: "0"});
+    $("nav").css({display: "block"});
     $("nav").css({height: "160px", transition: "height 1500ms"});
     $("ul").css({opacity: "1", transition: "opacity 1000ms"});
   }
