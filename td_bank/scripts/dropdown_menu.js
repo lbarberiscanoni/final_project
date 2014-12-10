42cc767610ee4394b9d04ce9f83ee809be29b302
lbarberiscanoni <hllbck7@gmail.com>
2014-12-10 07:54:36 -0500

    fixed the visibility bug on the dropdown script through the Sass. I could have used the .ready on the js but I went for a Sass change instead.

--- td_bank/scripts/dropdown_menu.js
+++ td_bank/scripts/dropdown_menu.js
@@ -1,12 +1,11 @@
 $(".menu").click(function(){
-	console.log("clicked");
-  if ($("nav").css("height") === "200px") {
+  if ($("nav").css("height") === "160px") {
     $("nav").css({height: "0"});
     $("ul").css({opacity: "0"});
     console.log("if works");
 
   } else {
-    $("nav").css({height: "200px", transition: "height 1000ms"});
+    $("nav").css({height: "160px", transition: "height 1500ms"});
     $("ul").css({opacity: "1", transition: "opacity 1000ms"});
     console.log("else works");
   }
