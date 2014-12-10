ddf7667a767fdd98b272f66e30969ee92abd3c52
lbarberiscanoni <hllbck7@gmail.com>
2014-12-10 08:35:01 -0500

    fixed the disapperaing text issue. Now the dropdown works perfectly

--- td_bank/scripts/dropdown_menu.js
+++ td_bank/scripts/dropdown_menu.js
@@ -1,12 +1,16 @@
 $(".menu").click(function(){
+  console.log("button works");
+
   if ($("nav").css("height") === "160px") {
+    console.log("if works");
     $("nav").css({height: "0"});
     $("ul").css({opacity: "0"});
-    console.log("if works");
+    $(".main").css({opacity: "1"});
 
   } else {
+    console.log("else works");
+    $(".main").css({opacity: "0"});
     $("nav").css({height: "160px", transition: "height 1500ms"});
     $("ul").css({opacity: "1", transition: "opacity 1000ms"});
-    console.log("else works");
   }
 });
