c159a56fb50fb2ce01203bba87b74fda507350c3
lbarberiscanoni <hllbck7@gmail.com>
2014-12-10 16:54:16 -0500

    started work on the transfers page

--- td_bank/scripts/dropdown_menu.js
+++ td_bank/scripts/dropdown_menu.js
@@ -7,10 +7,12 @@ $(".menu").click(function(){
     $("ul").css({opacity: "0", transition: "opacity 1000ms"});
     $("nav").css({display: "none"});
     $(".main").css({opacity: "1"});
+    $(".message_center").css({opacity: "1"});
 
   } else {
     console.log("else works");
     $(".main").css({opacity: "0"});
+    $(".message_center").css({opacity: "0"});
     $("nav").css({display: "block"});
     $("nav").css({height: "160px", transition: "height 1500ms"});
     $("ul").css({opacity: "1", transition: "opacity 1000ms"});
