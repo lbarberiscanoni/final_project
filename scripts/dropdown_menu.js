07468550f6ae78f8b54844d5201a6aa991f96c8a
lbarberiscanoni <hllbck7@gmail.com>
2014-12-11 13:35:24 -0500

    second restructuring of folders to make the project visible on gh-pages

---
+++ scripts/dropdown_menu.js
@@ -0,0 +1,20 @@
+$(".menu").click(function(){
+  console.log("button works");
+
+  if ($("nav").css("height") === "160px") {
+    console.log("if works");
+    $("nav").css({height: "0", transition: "height 1000ms"});
+    $("ul").css({opacity: "0", transition: "opacity 1000ms"});
+    $("nav").css({display: "none"});
+    $(".main").css({opacity: "1"});
+    $(".message_center").css({opacity: "1"});
+
+  } else {
+    console.log("else works");
+    $(".main").css({opacity: "0"});
+    $(".message_center").css({opacity: "0"});
+    $("nav").css({display: "block"});
+    $("nav").css({height: "160px", transition: "height 1500ms"});
+    $("ul").css({opacity: "1", transition: "opacity 1000ms"});
+  }
+});
