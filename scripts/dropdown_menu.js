0d343e03f7442e1a298633fa4d6edac76b6d3c67
lbarberiscanoni <hllbck7@gmail.com>
2014-12-15 13:16:27 -0500

    halfway through the responsiveness tweeks

--- scripts/dropdown_menu.js
+++ scripts/dropdown_menu.js
@@ -1,7 +1,7 @@
 $(".menu").click(function(){
   console.log("button works");
 
-  if ($("nav").css("height") === "160px") {
+  if ($("nav").css("height") === "250px") {
     console.log("if works");
     $("nav").css({height: "0", transition: "height 1000ms"});
     $("ul").css({opacity: "0", transition: "opacity 1000ms"});
@@ -14,7 +14,7 @@ $(".menu").click(function(){
     $(".main").css({opacity: "0"});
     $(".message_center").css({opacity: "0"});
     $("nav").css({display: "block"});
-    $("nav").css({height: "160px", transition: "height 1500ms"});
+    $("nav").css({height: "250px", transition: "height 1500ms"});
     $("ul").css({opacity: "1", transition: "opacity 1000ms"});
   }
 });
