56ed92272f061f28fb3ca557d7d066d3da049bdd
lbarberiscanoni <hllbck7@gmail.com>
2014-12-16 18:44:53 -0500

    finished the effects to manage alerts and styled all the dropdowns

--- scripts/show_hide.js
+++ scripts/show_hide.js
@@ -1,3 +1,16 @@
-$("button").click(function(){
-	$(".show").toggle();
+$("button.newAlert").click(function(){
+	$(".alert_new").toggleClass("show");
+});
+
+$("button.deactivate").click(function(){
+	$(".alert1").toggleClass("hide");
+	$(".alert2").toggleClass("hide");
+});
+
+$("button.newContact").click(function(){
+	$(".contact_new").toggleClass("show");
+});
+
+$("button.delete").click(function(){
+	$(this).closest("tr").remove();
 });
