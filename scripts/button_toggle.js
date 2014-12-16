d092232d5ad659ba318fbaee91d1f18e5766adc4
lbarberiscanoni <hllbck7@gmail.com>
2014-12-16 09:04:13 -0500

    added the final products content and implemented the script for it. Working on the layout of the bottom wrapper

---
+++ scripts/button_toggle.js
@@ -0,0 +1,47 @@
+$('.button-1').click(function(){
+  $('.hidden-1').toggle();
+  $('.hidden-2').hide();
+  $('.hidden-3').hide();
+  $('.hidden-4').hide();
+  $('.hidden-5').hide();
+});
+
+$('.button-2').click(function(){
+  $('.hidden-2').toggle();
+  $('.hidden-1').hide();
+  $('.hidden-3').hide();
+  $('.hidden-4').hide();
+  $('.hidden-5').hide();
+});
+
+$('.button-3').click(function(){
+  $('.hidden-3').toggle();
+  $('.hidden-1').hide();
+  $('.hidden-2').hide();
+  $('.hidden-4').hide();
+  $('.hidden-5').hide();
+});
+
+$('.button-4').click(function(){
+  $('.hidden-4').toggle();
+  $('.hidden-1').hide();
+  $('.hidden-2').hide();
+  $('.hidden-3').hide();
+  $('.hidden-5').hide();
+});
+
+$('.button-5').click(function(){
+  $('.hidden-5').toggle();
+  $('.hidden-1').hide();
+  $('.hidden-2').hide();
+  $('.hidden-3').hide();
+  $('.hidden-4').hide();
+});
+
+$('.button-6').click(function(){
+  $('.hidden-6').toggle();
+  $('.hidden-1').hide();
+  $('.hidden-2').hide();
+  $('.hidden-3').hide();
+  $('.hidden-4').hide();
+});
